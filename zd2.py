15de4d4ce6b1b2d2d8e6b6458a44c34e226bb59a
Danm3rr <90203898+VladVorushylo@users.noreply.github.com>
2021-11-28 23:31:27 +0200

    Update zd2.py

--- zd2.py
+++ zd2.py
@@ -1,1 +1,12 @@
+import matplotlib.pyplot as plt
+import pandas as pd
 
+
+t = 'Этот мир прогнил'
+list = list(t)
+x = pd.DataFrame({'letters': list})
+x['num'] = 1
+x = x.groupby('letters').sum().sort_values('num', ascending=False) / len(x)
+
+plt.bar(x.index, x.num, width=0.5)
+plt.show()
